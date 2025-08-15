2e8060fa44a8b71f36d86995c2decbfabf53a9c3
naosudo <nao@nicoriwa.com>
2025-08-15 14:54:38 +0900

    first commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,67 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  def index
+    @items = Item.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    redirect_to root_path unless current_user == @item.user
+  end
+  def update
+    # 自身の出品商品以外は編集不可
+    unless current_user == @item.user
+      redirect_to root_path and return
+    end
+
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "商品情報を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if current_user == @item.user
+      @item.destroy
+      redirect_to root_path, notice: "商品を削除しました"
+    else
+      redirect_to root_path, alert: "削除権限がありません"
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :image,
+      :price,
+      :description,
+      :category_id,
+      :status_id,
+      :payment_id,
+      :prefecture_id,
+      :day_id
+    ).merge(user_id: current_user.id)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  
+  root to: "items#index"
+end
